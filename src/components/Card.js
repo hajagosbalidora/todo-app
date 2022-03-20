ba602f04df09d58f8e282d2ebee93adf86b40cc1
hajagosbalidora <hajagosbali.dora@gmail.com>
2022-03-20 10:18:28 +0100

    addcardbutton

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import Task from './Task';
 
@@ -5,14 +6,22 @@ function Card(props){
     return(
         <div className="Card">
             <h2 className="head_card">{props.name}
-            <button className="addbutton">Add</button>
+            <button className="addbutton">Add
+            </button>
             </h2>
             <div>
                 <Task name = "Task one"/>
             </div>
         </div>
 
-    );
+    );   
+}
+function AddCardButton(){
+    return(
+        <div className="AddCardButton">
+            <button>Add Card</button>
+        </div>
+    )
 }
 
 export default Card;
