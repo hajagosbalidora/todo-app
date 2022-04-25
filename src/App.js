91ab02a120a29b0ee74c2f04bf347ba7281eb171
Hajagos-Bali Dóra <hajagosbali.dora@gmail.com>
2022-04-25 14:15:43 +0200

    takarítás, rendszerezés

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Card from './components/Card.js';
 import Task from './components/Task.js';
 
@@ -6,45 +6,45 @@ import Task from './components/Task.js';
 class App extends Component {
   state = {
     cards: [
-      {id:1, title:"terv"},
-      {id:2, title:"folyamatban"}
+      { id: 1, title: "terv" },
+      { id: 2, title: "folyamatban" }
     ]
-   
+
   }
 
-  cardDeleteHandler = (cardIndex) =>{
+  cardDeleteHandler = (cardIndex) => {
     const cards = this.state.cards;
     cards.splice(cardIndex, 1);
-    this.setState({cards: cards});
+    this.setState({ cards: cards });
   }
-  cardEditHandler = (cardIndex) =>{
+  cardEditHandler = (cardIndex) => {
     const cards = this.state.cards;
 
-    this.setState({cards: cards});
+    this.setState({ cards: cards });
   }
 
 
-  AddCardButtonHandler = () =>{
-    var newArray = this.state.cards.slice();    
-    newArray.push({id: 4, title: "Kész"});   
-    this.setState({cards:newArray})
-}
+  AddCardButtonHandler = () => {
+    var newArray = this.state.cards.slice();
+    newArray.push({ id: 4, title: "Kész" });
+    this.setState({ cards: newArray })
+  }
 
-  render (){
+  render() {
     return (
       <div className="App">
         <div className='App_header'>
-        <h1>Project</h1>
-        <button className='AppEditbutton'>Edit Project</button>
+          <h1>Project</h1>
+          <button className='AppEditbutton'>Edit Project</button>
         </div>
         <div className='CardsBox'>
           {
             this.state.cards.map((card, index) => {
-              return(
-                <Card 
-                title={card.title} 
-                delete={() => this.cardDeleteHandler(index)}
-                key={card.id}
+              return (
+                <Card
+                  title={card.title}
+                  delete={() => this.cardDeleteHandler(index)}
+                  key={card.id}
                 />
               )
             })
@@ -52,10 +52,10 @@ class App extends Component {
           <button className="AddCard_btn" onClick={() => this.AddCardButtonHandler()}>Add Card</button>
         </div>
       </div>
-      
+
     );
   }
-  
+
 
 }
 
--- src/components/Task.js
+++ src/components/Task.js
@@ -2,19 +2,19 @@ import React from 'react';
 import binlogo from "../image/bin_black.png";
 import pencillogo from "../image/pencil_black.png";
 
-function Task(props){
-    return(
+function Task(props) {
+    return (
         <div className='Task'>
             <h3 className='Task_header'>{props.title}
                 <div className='Buttons'>
-                    <button className="Deletebutton" onClick={props.delete}><img src={binlogo} alt="Delete this task"/></button>
-                    <button className="Editbutton"><img src={pencillogo} alt="Edit this card"/></button>
+                    <button className="Deletebutton" onClick={props.delete}><img src={binlogo} alt="Delete this task" /></button>
+                    <button className="Editbutton"><img src={pencillogo} alt="Edit this card" /></button>
                 </div>
             </h3>
         </div>
     );
 
-        
+
 }
 
 export default Task;
